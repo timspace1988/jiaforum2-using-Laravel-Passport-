d3404ee81762ce6cfae950ae76324609e06dd444
JIA <timspace1988@hotmail.com>
2019-12-23 13:15:18 +0000

    Registeration captcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,22 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                          <label for="captcha" class="col-md-4 col-form-label text-md-right">Captcha</label>
+
+                          <div class="col-md-6">
+                            <input id="captcha" class="form-control {{ $errors->has('captcha') ? 'is-invalid' : '' }}" name="captcha" required>
+
+                            <img src="{{ captcha_src('flat') }}" class="thumbnail captcha mt-3 mb-2" onclick="this.src='/captcha/flat?'+Math.random()" title="Click to get new captcha">
+
+                            @error('captcha')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                            @enderror
+                          </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
