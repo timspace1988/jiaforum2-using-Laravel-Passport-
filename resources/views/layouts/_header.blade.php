99593e74a8ee2ba90508ae6a3715456b6e498358
JIA <timspace1988@hotmail.com>
2019-12-23 12:05:49 +0000

    Modify home page redirect link

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -14,8 +14,28 @@
       <!--Right side of navbar-->
       <ul class="navbar-nav navbar-right">
         <!--Authentication link-->
+        @guest
         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Sign in</a></li>
         <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Sign up</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="ture" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a href="" class="dropdown-item">Account</a>
+            <a href="" class="dropdown-item">Edit Profile</a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item" id="logout">
+              <form action="{{ route('logout') }}" method="POST">
+                {{ csrf_field()}}
+                <button class="btn btn-block btn-danger" type="submit" name="button">Log Out</button>
+              </form>
+            </a>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
