68c852a66eaf44676dd7deeeee2f9d48aed003ac
JIA <timspace1988@hotmail.com>
2019-12-23 16:58:39 +0000

    Password reset notification display

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,14 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function __construct(){
+        $this->middleware('guest');
+    }
+
+    //overide the sendResetResponse function in trait "ResetsPasswords"
+    protected function sendResetResponse(Request $request, $response){
+        session()->flash('success', 'Password has been updated, you are signed in now.');
+        return redirect($this->redirectPath());
+    }
 }
