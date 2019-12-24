325029b685dc9d3e60905da38751754822d8cc54
JIA <timspace1988@hotmail.com>
2019-12-24 16:40:01 +0000

    Upload avatar

---
+++ app/Handlers/ImageUploadHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Handlers;
+
+use Illuminate\Support\Str;
+
+class ImageUploadHandler{
+    //only allow to upload images with following extensions
+    protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
+
+    public function save($file, $folder, $file_prefix){
+        //build folder structure
+        $folder_name = "upload/images/$folder/" . date("Ym/d", time());
+
+        //public_path() will get public folder's physical address
+        $upload_path = public_path() . '/' . $folder_name;
+
+        //the file's name from clipping board has no extension name, we need to add png to it
+        $extension = strtolower($file->getClientOriginalExtension()) ?:'png';
+
+        //add a prefix to filename, the prefix could be related model's id
+        $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+
+        //if the uploaded file is not a image, will terminate the operation
+        if(! in_array($extension, $this->allowed_ext)){
+            return false;
+        }
+
+        //move the image to the target storage directory
+        $file->move($upload_path, $filename);
+
+        //config('app.url') is public folder address on server, public_path() is the public folder physical address on local computer, e.g. /home/vagrant/Code/jiaforum/public
+        return ['path' => config('app.url') . "$folder_name/$filename"];
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -16,11 +17,26 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user){
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $data = $request->all();
+        //if $request contains file (the 'avatar' is the field name on form)
+        //$request->avatar will get that file
+        //ImageUploadhandler::save($file, $folder, $file_prefix)
+        if($request->avatar){
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            //in ImageUploadHandler, we make it return false if the extension is not image
+            //So, we need to check the $result
+            if($result){
+                //originally, $data['avatar'] is a file which cannot be stored in database,
+                //So, we need to change it to file's path
+                $data['avatar'] = $result['path'];
+            }
+        }
+
         //UserRequest extends the 'FormRequest'
         //we will use 'FormRequest' to validate the data
         //we design the rules it in UserRequest class (created by us)
-        $user->update($request->all());
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', 'Your profiles has been updated');
     }
 }
