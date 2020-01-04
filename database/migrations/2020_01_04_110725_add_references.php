896511a73bea67ec785c2ee0abd7296f40a1f1fd
JIA <timspace1988@hotmail.com>
2020-01-04 00:37:32 +0000

    Delete referenced user, topic and reply

---
+++ database/migrations/2020_01_04_110725_add_references.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddReferences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('topics', function(Blueprint $table){
+            //Build foreign key restriction  on topics table
+            //When user_id's user was deleted from users table, delete this topic
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('replies', function(Blueprint $table){
+            //Build foreign key restriction  on replies
+            //When user_id's user was deleted from users table, delete this reply
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            //When topic_id's topic was deleted from topics table, delete this reply
+            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('topics', function(Blueprint $table){
+            //Remove the foreign key restrition on topics table
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::table('topics', function(Blueprint $table){
+            //Remove the foreign key restrition on replies table
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['topic_id']);
+        });
+    }
+}
