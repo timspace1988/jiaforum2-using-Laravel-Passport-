a33f000ad09332a58a3bf3dbdebf57c35ced1136
JIA <timspace1988@hotmail.com>
2020-02-24 06:14:29 +0000

    Edit topic

--- app/Http/Controllers/Api/V1/TopicsController.php
+++ app/Http/Controllers/Api/V1/TopicsController.php
@@ -9,11 +9,22 @@ use App\Http\Requests\Api\V1\TopicRequest;
 
 class TopicsController extends Controller
 {
+    //Create a new topic
     public function store(TopicRequest $request, Topic $topic){
         $topic->fill($request->all());
         $topic->user_id = $request->user()->id;
         $topic->save();
 
         return new TopicResource($topic);
+    }
+
+    //Edit a current topic
+    public function update(TopicRequest $request, Topic $topic){
+        //Do the authorization using topic's update policy
+        $this->authorize('update', $topic);
+
+        $topic->update($request->all());
+        return new TopicResource($topic);
+
     }
 }
