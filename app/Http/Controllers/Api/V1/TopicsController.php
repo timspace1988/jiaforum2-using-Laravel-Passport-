0fa0817e128ec914c630887ff3a6c7762dcb9abc
JIA <timspace1988@hotmail.com>
2020-03-07 11:27:25 +0000

    Topic detail

--- app/Http/Controllers/Api/V1/TopicsController.php
+++ app/Http/Controllers/Api/V1/TopicsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Queries\TopicQuery;
 use App\Http\Resources\TopicResource;
 use App\Http\Requests\Api\V1\TopicRequest;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -41,7 +42,7 @@ class TopicsController extends Controller
     }
 
     //Get topics list
-    public function index(Request $request, Topic $topic){
+    public function index(Request $request, TopicQuery $query){
         // $query = $topic->query();
 
         // if($categoryId = $request->category_id){
@@ -51,32 +52,51 @@ class TopicsController extends Controller
         // $topics = $query->with('user', 'category')->withOrder($request->order)->paginate();
 
 
-        $topics = QueryBuilder::for(Topic::class)
-        //'user' and 'category' are relationship defined in Topic model
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',//contains xx
-                AllowedFilter::exact('category_id'),//exactly matches xx
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        // $topics = QueryBuilder::for(Topic::class)
+        // //'user' and 'category' are relationship defined in Topic model
+        //     ->allowedIncludes('user', 'category')
+        //     ->allowedFilters([
+        //         'title',//contains xx
+        //         AllowedFilter::exact('category_id'),//exactly matches xx
+        //         AllowedFilter::scope('withOrder')->default('recentReplied'),
+        //     ])
+        //     ->paginate();
+
+        $topics = $query->paginate();
 
         return TopicResource::collection($topics);
     }
 
     //Get a user's posts(topics) list
-    public function userIndex(Request $request, User $user){
-        $query = $user->topics()->getQuery();
+    public function userIndex(Request $request, User $user, TopicQuery $query){
+        // $query = $user->topics()->getQuery();
 
-        $topics = QueryBuilder::for($query)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        // $topics = QueryBuilder::for($query)
+        //     ->allowedIncludes('user', 'category')
+        //     ->allowedFilters([
+        //         'title',
+        //         AllowedFilter::exact('category_id'),
+        //         AllowedFilter::scope('withOrder')->default('recentReplied'),
+        //     ])
+        //     ->paginate();
 
+        $topics = $query->where('user_id', $user->id)->paginate();
         return TopicResource::collection($topics);
+    }
+
+    // //Show a topic's detail (method 1)
+    // public function show(Topic $topic){
+    //     return new TopicResource($topic);
+    // }
+
+    //Show a topic's detail (method 2)
+    public function show($topicId, TopicQuery $query){
+        // $topic = QueryBuilder::for(Topic::class)
+        //     ->allowedIncludes('user', 'category')
+        //     ->findOrFail($topicId);
+
+        $topic = $query->findOrFail($topicId);
+
+        return new TopicResource($topic);
     }
 }
